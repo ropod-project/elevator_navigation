d0a864e66423c813258493c362b34b92c1a2d173
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-07-16 13:46:08 +0200

    Populated the feedback msgs published by the actions

--- ros/src/elevator_navigation/elevator_nav_utils.py
+++ ros/src/elevator_navigation/elevator_nav_utils.py
@@ -3,7 +3,7 @@ from tf import transformations as tf
 
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from maneuver_navigation.msg import Goal as ManeuverNavigationGoal
-
+from ropod_ros_msgs.msg import NavElevatorFeedback, Status
 
 class ElevatorNavData(object):
     def __init__(self):
@@ -14,6 +14,24 @@ class ElevatorNavData(object):
         self.elevator_id = -1
         self.base_pose = PoseWithCovarianceStamped()
 
+def get_feedback_msg_skeleton(action_id, action_type):
+    '''Returns a NavElevatorFeedback message with the following fields prefilled:
+    * feedback.action_id
+    * feedback.type
+    * feedback.status.domain, and
+    * feedback.status.module_code
+
+    Keyword arguments:
+    action_id: str -- ID of the action
+    action_type: str -- elevator action type
+
+    '''
+    feedback_msg = NavElevatorFeedback()
+    feedback_msg.feedback.action_id = action_id
+    feedback_msg.feedback.action_type = action_type
+    feedback_msg.feedback.status.domain = Status.COMPONENT
+    feedback_msg.feedback.status.module_code = Status.ELEVATOR_ACTION
+    return feedback_msg
 
 def has_timed_out(start_time, timeout):
     '''Returns True if the difference between the current time and the given
